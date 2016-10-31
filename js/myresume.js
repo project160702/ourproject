d510892230abe6b6c5716d11d43fdceb9e0cddd9
chenxi-chenija <709207209@qq.com>
2016-10-31 14:18:31 +0800

    jquery修改

--- js/myresume.js
+++ js/myresume.js
@@ -1,8 +1,8 @@
 $(function(){
-	$("a").attr("href","javascript:;").attr("rel","noopener noreferrer")
+	$("a").attr("href","javascript:;").attr("rel","noopener noreferrer");
 	$("#zhe").css("height",$("body").height());
 	$("#box .sjzj .contenter .contenter2 .right .xh a").click(function(){
-		$(".tankuang").addClass('tankuangk')
+		$("#zhe,#zhes").addClass('tankuang').addClass('tankuangk');
 	})
 	$("#zhe").click(function(){
 		$(".tankuangk").removeClass('tankuangk')
